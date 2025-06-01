505d2fda2b0f880e9f941979b98fd2d0c2081d38
avri <avvritr@gmail.com>
2025-06-01 13:23:10 -0300

    main

--- assets/db/bdHarcodeados.js
+++ assets/db/bdHarcodeados.js
@@ -2,19 +2,29 @@ const baseDeDatos = [
     {
         categoria: "tortas",
         productos:[
-            {},
-            {},
-            {},
+            {nom: "Cheesecake",precio: 40000},
+            {nom: "Lemonpie",precio: 40000},
+            {nom: "Carrotcake",precio: 40000},
+            {nom: "Brownie",precio: 40000},
+            {nom: "Pastafrola",precio: 40000}
+        ]
+    },
+    {
+        categoria: "cookies",
+        productos:[
+            {nom: "Clásica",precio: 4000},
+            {nom: "Brownie",precio: 4000},
+            {nom: "Kinder",precio: 5000},
+            {nom: "Pistacho y chocoblanco",precio: 5000}
 
         ]
     },
     {
         categoria: "budines",
         productos:[
-            {},
-            {},
-            {},
-
+            {nom: "Banana",precio: 40000},
+            {nom: "Limón y amapola",precio: 40000},
+            {nom: "Vainilla y chips",precio: 40000}
         ]
     }
 ]
